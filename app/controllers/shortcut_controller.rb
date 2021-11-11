5b6aaadb915fec5aa19134ee013f139563693397
Dalen Ward <dalen.ward@m6securities.com>
2021-11-11 11:15:24 -0700

    will redirect shortened links

--- app/controllers/shortcut_controller.rb
+++ app/controllers/shortcut_controller.rb
@@ -1,7 +1,11 @@
 class ShortcutController < ApplicationController
 
   def link
+    shortcut = params[:shortcut]
 
+    shortlink = Shortlink.find_by shortcut: shortcut
+
+    redirect_to shortlink.url
   end
 
 end
