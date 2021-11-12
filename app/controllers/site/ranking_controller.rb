cc62aba6f7411e797512bd39bec7c2ab51520c9d
Dalen Ward <dalen.ward@m6securities.com>
2021-11-11 18:22:15 -0700

    added header

---
+++ app/controllers/site/ranking_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Site
+  class RankingController < SiteController
+
+    def index
+      @top = nil
+      @new = nil
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     match '/create_shortlink' => 'home#create_shortlink', via: %i[post put patch]
 
     resources :shortlink_details, only: :show
+    resources :ranking, only: :index
   end
 
   # this must be the very last
